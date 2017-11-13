24959b1b6288f0e02b72afbcdb22a565d90ab3da
datawolfalpha <shaanshivsuleman@gmail.com>
2017-11-14 01:16:10 +0530

    *
    Signed-off-by: datawolfalpha <shaanshivsuleman@gmail.com>

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,3 +1,11 @@
-$(".toggle").on("click", function() {
-  $(".toggle").parent().toggleClass('active');
+/*Navbar*/
+$(".toggle").on("click", function () {
+	$(".toggle").parent().toggleClass('active');
 });
+
+/*Input Field Formatting*/
+document.getElementById("regno").addEventListener('input', function (e) {
+	e.target.value = e.target.value.replace(/[^\d0-9a-zA-Z]/g, '').replace(/^(.{2})(.{2})(.{2})(.{4})$/g, "$1 $2 $3 $4").trim();
+});
+
+/*Input Maxlength Auto Change*/
