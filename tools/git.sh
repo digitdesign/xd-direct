9e5aeeb267fe435acafae86692aecea8f5f928df
Shaan Shivanandan <shaanshivsuleman@gmail.com>
2017-12-05 04:47:47 +0530

    *
    Signed-off-by: Shaan Shivanandan <shaanshivsuleman@gmail.com>

--- tools/git.sh
+++ tools/git.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
-git status -s;git add .;echo -n "→ ";read COMMITMSG;git commit -am "$COMMITMSG" -s;git push origin master -q;clear;exit
+
+set -o errexit
+set -o nounset
+set -o pipefail
+trap 'echo; echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
+
+git status -s
+git add .
+echo -n "→ "
+read COMMITMSG
+git commit -am "$COMMITMSG" -s
+git push origin master -q
+clear
+exit
