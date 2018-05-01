de08cfbba3143a2017d487a5b7a84a74c7b22fdf
Shaan Shivanandan <shaan.shivanandan@godigit.com>
2018-05-01 16:26:11 +0530

    Refactor: Removed redundant JS code
    Signed-off-by: Shaan Shivanandan <shaan.shivanandan@godigit.com>

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -3,18 +3,6 @@
 // Navbar
 // ––––––––––––––––––––––––––––––––––––––––––––––––––
 document.getElementsByClassName('.toggle').onclick(function () {
-	document.getElementsByClassName('.toggle').parent.toggleClass("active");
-});
-
-// Image ALT text
-// ––––––––––––––––––––––––––––––––––––––––––––––––––
-$("img:not([alt])").setAttribute("alt", "");
-
-// External link treatment
-// ––––––––––––––––––––––––––––––––––––––––––––––––––
-$("a").filter(function () {
-	return this.hostname && this.hostname !== location.hostname;
-}).setAttribute({
-	target: "_blank",
-	rel: "nofollow noreferrer noopener"
+	let navButton = document.getElementsByClassName('.toggle').parent;
+    navButton.classList.toggle("active")
 });
